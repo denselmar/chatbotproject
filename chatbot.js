b8ed2ea5044368e20ce569f27c40019fa547df01
Densel Martin <denselmartin@gmail.com>
2016-09-28 14:51:59 -0700

    pt.4
    fallback response

--- chatbot.js
+++ chatbot.js
@@ -1,18 +1,24 @@
 var responses = {
   "Hi": "Hello",
-  "How are you": "Good",
+  "How are you?": "Good",
+  "How old are you?": "I dont know to be honest",
+  "Where do you live?": "In your computer",
+  "What is your name?": "I don't have one make use you imagination",
 }
+
+var fallbacks = ["ask another question", "what", "sure", "ok"];
 
 function speak() {
 
   var userInput = $("#input").val();
   var response = responses[userInput];
 
-  $('#chat-area').prepend(response + "</br>");
-  $('#chat-area').prepend(userInput + "</br>");
+if(response == undefined) {
+  $('#chat-area').prepend(fallbacks[0] + "..." + "</br>");
 }
-
-
-
-
-i hate life
+else{
+  $('#chat-area').prepend(response + "</br>");
+}
+  $('#chat-area').prepend(userInput + "</br>");
+ugh
+}
